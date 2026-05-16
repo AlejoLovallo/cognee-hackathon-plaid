cefd13c5436cbbddcecdfb789cfd2678019fc415
Mohinsh Sai Prasad <mohnish2401@gmail.com>
2026-05-16 14:48:32 -0700

    added redis to cli

--- src/cognee_cli/cli.py
+++ src/cognee_cli/cli.py
@@ -1,5 +1,11 @@
 import click
+from cognee_community_vector_adapter_redis import register
+from cognee import config
 
+config.set_vector_db_config({
+    "vector_db_provider": "redis",
+    "vector_db_url": "redis://localhost:6379",
+})
 from .commands import ingest, query, lint
 
 
